88e3f334c72aa9f0078438045fad9065b7d55916
Dis <397465+disconn3ct@users.noreply.github.com>
2022-09-19 11:26:56 -0400

    fix: lots
    - keepDays is required, don't randomly run off deleting crap if it
      is partly unconfigured
    - use the same readability test for the config file everywhere
    - move effective-config output to after parsing is complete
    - reapply config file after parsing.
      - per docs, it overrides all but it wasn't being reread before
      - this also solves -f being a NOOP
      - WARN: technically a race condition between 2 "is it readable" tests,
      turns it back into NOOP
    - fix help typo for appid (`-a` not `-l`)
    - useful return codes from http errors
    - single-quote ? case to work around escape issues downstream
    - prettify and manually reformat a bunch

--- gotify-delete-old-notifications.sh
+++ gotify-delete-old-notifications.sh
@@ -10,94 +10,101 @@ GotifyApplicationId=10
 # Gotify Client Token
 GotifyClientToken="xxxxxxxxxx"
 # Days to keep Notifications
-keepDays=7
+# keepDays=7
 
 CentralConfigFile="/etc/gotify-delete-old-notifications.conf"
 
 ### END OF CONFIGURATION ###
 
-
-if [[ -f "$CentralConfigFile" ]]; then
-
+if [[ -r "$CentralConfigFile" ]]; then
 	. $CentralConfigFile
-
 fi
+
+PrintConfig=0
 
 while getopts ":hd:a:c:k:gsf:" option; do
 	case $option in
-		h)	# Help
-			echo "Simple Gotify old notifications cleaner."
-			echo "By Georgiy Sitnikov."
-			echo ""
-			echo "Usage: ./gotify-delete-old-notifications.sh [options]"
-            echo ""
-			echo "Example:"
-            echo "For Application ID 10:"
-			echo "  ./gotify-delete-old-notifications.sh -a 10"
-            echo ""
-            echo "... save messages for last 7 days,
+	h) # Help
+		echo "Simple Gotify old notifications cleaner."
+		echo "By Georgiy Sitnikov."
+		echo ""
+		echo "Usage: ./gotify-delete-old-notifications.sh [options]"
+		echo ""
+		echo "Example:"
+		echo "For Application ID 10:"
+		echo "  ./gotify-delete-old-notifications.sh -a 10"
+		echo ""
+		echo "... save messages for last 7 days,
     on custom server:"
-			echo "  ./gotify-delete-old-notifications.sh -a 10 -k 7 -d server.com/gotify"
-            echo ""
-            echo "Delete all messages older than 1 year, aka 'globally':"
-			echo "  ./gotify-delete-old-notifications.sh -g -k 365"
-			echo ""
-			echo "Options:"
-			echo "  -l <ID>         Set Gotify Application ID. E.g. '10' than messages only
+		echo "  ./gotify-delete-old-notifications.sh -a 10 -k 7 -d server.com/gotify"
+		echo ""
+		echo "Delete all messages older than 1 year, aka 'globally':"
+		echo "  ./gotify-delete-old-notifications.sh -g -k 365"
+		echo ""
+		echo "Options:"
+		echo "  -a <ID>         Set Gotify Application ID. E.g. '10' than messages only
                   for application 10 will be deleted."
-			echo "  -k <Number>     Set Number of days to keep messages, all messages older than
+		echo "  -k <Number>     Set Number of days to keep messages, all messages older than
                   this number will be deleted.
                   Could not be zero. Default 7."
-			echo "  -g              Work Globally for all applications, could not be combined with '-a'."
-			echo "  -c              Set Client Token for Gotify."
-			echo "  -d              Set Gotify Path and Port as: 'server.com:8080/gotify'."
-            echo "                  HTTPS is only supported protocol, you do not need to set it."
-			echo "  -f <path>       Custom path to config file, default '/etc/gotify-delete-old-notifications.conf'."
-			echo "  -s              Show current active configuration and exit."
-			echo "  -h              This help."
-			exit 0
-			;;
-		d)	# set GotifyDomain
-			GotifyDomain="$OPTARG"
-			;;
-		a)	# set GotifyApplicationId
-			if [[ "$OPTARG" == "" ]]; then
-
-				echo "You have to set Gotify Application ID, or use '-g' instead. Try '-h' for help"
-				exit 1
-
-			else
-
-				GotifyApplicationId="$OPTARG"
-
-			fi
-			;;
-		c)	# set GotifyClientToken
-			GotifyClientToken="$OPTARG"
-			;;
-		k)	# set keepDays
-			keepDays="$OPTARG"
-			;;
-		g)	# unset GotifyApplicationId to work globally
-			GotifyApplicationId=""
-			;;
-		f)	# set CentralConfigFile
-			CentralConfigFile="$OPTARG"
-			[[ -r "$CentralConfigFile" ]] || { echo "ERROR - Custom config file is set, but not could not be read under $CentralConfigFile."; exit 1; }
-			;;
-		s)	# Show current Active Configuration
-			echo "Gotify Path:                 $GotifyDomain"
-			echo "Gotify Application ID:       $GotifyApplicationId"
-			echo "Gotify Client Token:         $(echo $GotifyClientToken | cut -c -4)..."
-			echo "Days to keep messages:       $keepDays"
-			echo "Expected configuration file: $CentralConfigFile"
-			exit 0
-			;;
-		\?)
-			break
-			;;
+		echo "  -g              Work Globally for all applications, could not be combined with '-a'."
+		echo "  -c              Set Client Token for Gotify."
+		echo "  -d              Set Gotify Path and Port as: 'server.com:8080/gotify'."
+		echo "                  HTTPS is only supported protocol, you do not need to set it."
+		echo "  -f <path>       Custom path to config file, default '/etc/gotify-delete-old-notifications.conf'."
+		echo "  -s              Show current active configuration and exit."
+		echo "  -h              This help."
+		exit 0
+		;;
+	d) # set GotifyDomain
+		GotifyDomain="$OPTARG"
+		;;
+	a) # set GotifyApplicationId
+		if [[ "$OPTARG" == "" ]]; then
+			echo "You have to set Gotify Application ID, or use '-g' instead. Try '-h' for help"
+			exit 1
+		else
+			GotifyApplicationId="$OPTARG"
+		fi
+		;;
+	c) # set GotifyClientToken
+		GotifyClientToken="$OPTARG"
+		;;
+	k) # set keepDays
+		keepDays="$OPTARG"
+		;;
+	g) # unset GotifyApplicationId to work globally
+		GotifyApplicationId=""
+		;;
+	f) # set CentralConfigFile
+		CentralConfigFile="$OPTARG"
+		[[ -r "$CentralConfigFile" ]] || {
+			echo "ERROR - Custom config file is set, but not could not be read under $CentralConfigFile."
+			exit 1
+		}
+		;;
+	s)
+		PrintConfig=1
+		;;
+	'?')
+		break
+		;;
 	esac
 done
+
+if [[ -r "$CentralConfigFile" ]]; then
+	. $CentralConfigFile
+fi
+
+if [[ "$PrintConfig" == 1 ]]; then
+	# Show current Active Configuration
+	echo "Gotify Path:                 $GotifyDomain"
+	echo "Gotify Application ID:       $GotifyApplicationId"
+	echo "Gotify Client Token:         $(echo $GotifyClientToken | cut -c -4)..."
+	echo "Days to keep messages:       $keepDays"
+	echo "Expected configuration file: $CentralConfigFile"
+	exit 0
+fi
 
 # Set curl Options, like timeout and number of retries
 curlConfiguration="-fsS -m 10 --retry 3"
@@ -129,11 +136,16 @@ Paging=""
 
 # Connectivity check
 connectivityCheck="$(curl -m 10 -sL -w "%{http_code}\n" "$GotifyURL?token=$GotifyClientToken" -o /dev/null)"
-[[ "$connectivityCheck" == "401" ]] && { echo "ERROR - Unauthorized. Please check Client Token."; exit 1; }
-[[ "$connectivityCheck" == "000" ]] && { echo "ERROR - Host not reacheble under https://$GotifyDomain. Please check if Server and Port are correct."; exit 1; }
+[[ "$connectivityCheck" == "401" ]] && {
+	echo "ERROR - Unauthorized. Please check Client Token."
+	exit 401
+}
+[[ "$connectivityCheck" == "000" ]] && {
+	echo "ERROR - Host not reacheble under https://$GotifyDomain. Please check if Server and Port are correct."
+	exit 500
+}
 
-getAllNotifications () {
-
+getAllNotifications() {
 	# Collect Notifications IDs and dates pushed to Gotify
 	apiCall="$(curl $curlConfiguration "$GotifyURL?token=$GotifyClientToken$Paging" 2>/dev/null)"
 
@@ -150,52 +162,34 @@ getAllNotifications () {
 	pagingLimit="$(echo $apiCall | jq .paging.limit)"
 	# Will be "0" if no following pages needed
 	pagingSince="$(echo $apiCall | jq .paging.since)"
-
 }
 
-deleteNotification () {
-
-	curl $curlConfiguration -X DELETE "$GotifyURLToDelete/$toDelete" -H "X-Gotify-Key:$GotifyClientToken" >> /dev/null
+deleteNotification() {
+	curl $curlConfiguration -X DELETE "$GotifyURLToDelete/$toDelete" -H "X-Gotify-Key:$GotifyClientToken" >>/dev/null
 	toDelete=""
-
 }
 
-findExpiredNotifications () {
-
+findExpiredNotifications() {
 	COUNT=1
-
 	while read notificationDate; do
-
 		if [[ "$DateToDeleteNotifications" > "$notificationDate" ]]; then
-
 			# Get Notification ID per following Number corresponding Date Following Number
 			toDelete="$(echo $getIDs | awk '{ print $'$COUNT' }')"
 			deleteNotification
-
 		fi
-
-		((COUNT++));
-
-	done <<< "$getDates" # Read from variable
-
+		((COUNT++))
+	done <<<"$getDates" # Read from variable
 }
 
-while :
-do
-
+while :; do
 	getAllNotifications
 	findExpiredNotifications
 
 	if [[ "$pagingSince" != 0 ]] && [[ "$pagingSince" != "null" ]]; then
-
 		Paging="&limit=$pagingLimit&since=$pagingSince"
-
 	else
-
 		break
-
 	fi
-
 done
 
 exit 0
